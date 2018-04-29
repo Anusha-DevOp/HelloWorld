c6a973572a3085f8b1ace1414d739c561913d10b
Anusha DevOp <anusha.sharma3010@gmail.com>
2018-04-29 22:43:16 +0530

    modified file added

--- Project2.java
+++ Project2.java
@@ -1,10 +1,10 @@
-public class helloj{
+public class project2{
 
 	public static void main(String[] args)
 	{	
 		for(int i=1;i<=10;i++)
 			{
-				System.out.println("Project Java23!!");
+				System.out.println("Project Java!!");
 			}
 	}
 }
